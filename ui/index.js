a597ef16fb34665ae7707e3b126c657e533124ca
Aki Salmi <aki.salmi@iki.fi>
2025-10-05 15:29:04 +0300

    ➖ UI: removed msw as prod dependency.

--- ui/index.js
+++ ui/index.js
@@ -1,10 +1,10 @@
 import {Shell} from '@rinkkasatiainen/cfb-shell'
 import {SessionDiscoveryModule} from '@rinkkasatiainen/cfb-session-discovery'
-import {startMSW} from './assets/scripts/browser.js'
+// import {startMSW} from './assets/scripts/browser.js'
 
 // Initialize MSW for development
 if (import.meta.env?.DEV || window.location.hostname === 'localhost') {
-  await startMSW()
+  // await startMSW()
 }
 
 const shell = new Shell()
